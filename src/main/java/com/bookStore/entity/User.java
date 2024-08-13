3c077095fbb46f8c7743dac0e3ac941cfb37d2b8
Minh Hải <117704352+abuhmhai@users.noreply.github.com>
2024-08-13 16:09:40 +0700

    update repository

--- src/main/java/com/bookStore/entity/User.java
+++ src/main/java/com/bookStore/entity/User.java
@@ -1,14 +1,12 @@
 package com.bookStore.entity;
 
-// File: User.java
-
-import javax.management.relation.Role;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -25,4 +23,36 @@ public class User {
     private Set<Role> roles = new HashSet<>();
 
     // Getters and Setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
+    }
 }
--- src/main/java/com/bookStore/service/UserService.java
+++ src/main/java/com/bookStore/service/UserService.java
@@ -1,8 +1,9 @@
 package com.bookStore.service;
-// File: UserService.java
 
 import com.bookStore.entity.User;
+import com.bookStore.entity.Role;  // Correct import for Role entity
 import com.bookStore.repository.RoleRepository;
+import com.bookStore.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -21,10 +22,16 @@ public class UserService {
 
     public void saveUser(User user) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
+
+        // Fetching the "GUEST" role from the database
         Role role = roleRepository.findByName("GUEST");
+
+        // Adding the role to the user's roles set
         user.getRoles().add(role);
+
+        // Saving the user in the database
         userRepository.save(user);
     }
 
-    // Additional methods
+    // Additional methods (like findByUsername, etc.)
 }
