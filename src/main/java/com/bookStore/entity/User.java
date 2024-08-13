ee831cfdc1e4fe64a3bce64aa43760f672473815
Minh Hải <117704352+abuhmhai@users.noreply.github.com>
2024-08-13 15:28:36 +0700

    Create User.java

---
+++ src/main/java/com/bookStore/entity/User.java
@@ -0,0 +1,28 @@
+package com.bookStore.entity;
+
+// File: User.java
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles = new HashSet<>();
+
+    // Getters and Setters
+}
