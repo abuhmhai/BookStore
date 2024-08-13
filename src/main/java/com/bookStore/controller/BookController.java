44cedf9fd1e3a02de98456ea46c642d4d1de3ace
Minh Hải <117704352+abuhmhai@users.noreply.github.com>
2024-08-13 15:40:24 +0700

    Update BookController.java

--- src/main/java/com/bookStore/controller/BookController.java
+++ src/main/java/com/bookStore/controller/BookController.java
@@ -1,6 +1,7 @@
 package com.bookStore.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -15,62 +16,66 @@ import java.util.*;
 
 @Controller
 public class BookController {
-	
+
 	@Autowired
 	private BookService service;
-	
+
 	@Autowired
 	private MyBookListService myBookService;
-	
+
 	@GetMapping("/")
 	public String home() {
 		return "home";
 	}
-	
+
 	@GetMapping("/book_register")
+	@PreAuthorize("hasRole('ADMIN')")
 	public String bookRegister() {
 		return "bookRegister";
 	}
-	
+
 	@GetMapping("/available_books")
 	public ModelAndView getAllBook() {
-		List<Book>list=service.getAllBook();
-//		ModelAndView m=new ModelAndView();
-//		m.setViewName("bookList");
-//		m.addObject("book",list);
-		return new ModelAndView("bookList","book",list);
+		List<Book> list = service.getAllBook();
+		return new ModelAndView("bookList", "book", list);
 	}
-	
+
 	@PostMapping("/save")
+	@PreAuthorize("hasRole('ADMIN')")
 	public String addBook(@ModelAttribute Book b) {
 		service.save(b);
 		return "redirect:/available_books";
 	}
+
 	@GetMapping("/my_books")
-	public String getMyBooks(Model model)
-	{
-		List<MyBookList>list=myBookService.getAllMyBooks();
-		model.addAttribute("book",list);
+	@PreAuthorize("hasAnyRole('ADMIN', 'GUEST')")
+	public String getMyBooks(Model model) {
+		List<MyBookList> list = myBookService.getAllMyBooks();
+		model.addAttribute("book", list);
 		return "myBooks";
 	}
+
 	@RequestMapping("/mylist/{id}")
+	@PreAuthorize("hasAnyRole('ADMIN', 'GUEST')")
 	public String getMyList(@PathVariable("id") int id) {
-		Book b=service.getBookById(id);
-		MyBookList mb=new MyBookList(b.getId(),b.getName(),b.getAuthor(),b.getPrice());
+		Book b = service.getBookById(id);
+		MyBookList mb = new MyBookList(b.getId(), b.getName(), b.getAuthor(), b.getPrice());
 		myBookService.saveMyBooks(mb);
 		return "redirect:/my_books";
 	}
-	
+
 	@RequestMapping("/editBook/{id}")
-	public String editBook(@PathVariable("id") int id,Model model) {
-		Book b=service.getBookById(id);
-		model.addAttribute("book",b);
+	@PreAuthorize("hasRole('ADMIN')")
+	public String editBook(@PathVariable("id") int id, Model model) {
+		Book b = service.getBookById(id);
+		model.addAttribute("book", b);
 		return "bookEdit";
 	}
+
 	@RequestMapping("/deleteBook/{id}")
-	public String deleteBook(@PathVariable("id")int id) {
+	@PreAuthorize("hasRole('ADMIN')")
+	public String deleteBook(@PathVariable("id") int id) {
 		service.deleteById(id);
 		return "redirect:/available_books";
 	}
-	
 }
