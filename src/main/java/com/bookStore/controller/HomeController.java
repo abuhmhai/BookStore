6ceb53af42cf8bc231a78e3c20dc828ccaf2872a
Minh Hải <117704352+abuhmhai@users.noreply.github.com>
2024-08-17 23:57:50 +0700

    Update HomeController.java

--- src/main/java/com/bookStore/controller/HomeController.java
+++ src/main/java/com/bookStore/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.bookStore.controller;
 
+import com.bookStore.model.UserDtls;
 import com.bookStore.service.UserService;
 import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -31,8 +32,17 @@ public class HomeController {
     @PostMapping("/createUser")
     public String createuser(@ModelAttribute User user)
     {
-        System.out.println(user);
-        return "register";
+        //System.out.println(user);
+        UserDtls userDtls =userService.createUser(user);
+        if(userDtls!=null)
+        {
+            System.out.println("Register Successfully");
+        }
+        else{
+            System.out.println("Someting went wrong");
+
+        }
+        return "redirect:/register";
     }
 
 }
